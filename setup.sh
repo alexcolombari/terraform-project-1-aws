756998de218e20d43161e96f8adf84076f4a3035
Alex <alexluiscolombari@gmail.com>
2026-03-20 02:20:22 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
-# Main script to run Docker, initialize Terraform and apply the infrastructure
-docker run -d -p 4566:4566 localstack/localstack
-sleep 10
+#!/bin/bashrc
+# Main script to run Docker, initialize Terraform and apply the infrastructure~
+echo "Starting LocalStack..."
+docker run -d --rm -p 4566:4566 -p 4510-4559:4510-4559 --name localstack_terraform localstack/localstack
+
+echo "Waiting..."
+sleep 15
+
+echo "Init Terraform..."
 terraform init
+
+echo "Applying Infrastructure..."
 terraform apply -auto-approve
+
+echo "Environment ready!"
